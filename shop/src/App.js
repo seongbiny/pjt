601b1c29b2a01433f07abc0da1f024a032646869
yunseongbin <s30ngb1n@gmail.com>
2022-01-09 01:30:59 +0900

    220108

--- shop/src/App.js
+++ shop/src/App.js
@@ -4,23 +4,25 @@ import { Navbar,Container,Nav,NavDropdown,Button } from 'react-bootstrap';
 import './App.css';
 import Data from './data.js';
 import Detail from './Detail.js';
+import axios from 'axios';
 
 import { Link, Route, Switch } from 'react-router-dom';
 
 function App() {
 
   let [shoes, shoes변경] = useState(Data);
+  let [재고, 재고변경] = useState([10, 11, 12]);
 
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">ShoeShpo</Navbar.Brand>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link> <Link to="/">Home</Link> </Nav.Link>
-              <Nav.Link> <Link to="/detail">Detail</Link> </Nav.Link>
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/detail">Detail</Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -36,28 +38,38 @@ function App() {
       
       <Switch>
         <Route exact path="/">
-          <div>
-            <div className="jumbotron">
-              <h1>20% Season Off</h1>
-              <p>안녕하세요</p>
-              <p>
-                <Button variant="primary">Learn more</Button>
-              </p>
+          
+          <div className="jumbotron">
+            <h1>20% Season Off</h1>
+            <p>안녕하세요</p>
+            <p>
+              <Button variant="primary">Learn more</Button>
+            </p>
+          </div>
+          <div className="container">
+            <div className="row">
+              {
+                shoes.map((a,i)=>{
+                  return <Card shoes={shoes[i]} i={i}/>
+                })
+              }
             </div>
-            <div className="container">
-              <div className="row">
-                {
-                  shoes.map((a,i)=>{
-                    return <Card shoes={shoes[i]} i={i}/>
-                  })
-                }
-              </div>
-            </div>
+            <button className="btn btn-primary" onClick={()=>{
+
+              axios.get('https://codingapple1.github.io/shop/data2.json')
+              .then((result)=>{
+                console.log(result.data)
+                shoes변경([...shoes, ...result.data]);
+              })
+              .catch(()=>{
+                console.log('실패')
+              })
+            }}>더보기</button>
           </div>
         </Route>
 
         <Route path="/detail/:id">
-          <Detail shoes={shoes}/>
+          <Detail shoes={shoes} 재고={재고} 재고변경={재고변경}/>
         </Route>
 
         
--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,21 +1,59 @@
-import React, { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import './Detail.scss';
+
+let 박스 = styled.div`
+    padding: 20px;
+
+`;
+
+let 제목 = styled.h4`
+    font-size : 25px;
+    color : ${ props => props.색상 }
+`;
 
 function Detail(props){
+    
+    let [alert, alert변경] = useState(true);
+    useEffect(()=>{
+        let 타이머 = setTimeout(()=>{ alert변경(false) }, 2000);
+        return ()=>{clearTimeout(타이머)}
+    }, [])
 
+    let [inputData, inputData변경] = useState('');
+
+    let { id } = useParams();
+    let 찾은상품 = props.shoes.find(function(상품){
+        return 상품.id == id
+    });
     let history = useHistory();
 
     return(
         <div className="container">
+            <박스>
+                <제목 className="red">Detail</제목>
+            </박스>
+            { inputData }
+            <input onChange={(e)=>{ inputData변경(e.target.value) }}/>
+            {
+                alert === true 
+                ? (<div className="my-alert2">
+                    <p>재고가 얼마 남지 않았습니다.</p>
+                    </div>) : null
+            }
+            
             <div className="row">
                 <div className="col-md-6">
                     <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
                 </div>
                 <div className="col-md-6 mt-4">
-                    <h4 className="pt-5">{props.shoes[0].title}</h4>
-                    <p>{props.shoes[0].content}</p>
-                    <p>{props.shoes[0].price}원</p>
-                    <button className="btn btn-danger">주문하기</button> 
+                    <h4 className="pt-5">{찾은상품.title}</h4>
+                    <p>{찾은상품.content}</p>
+                    <p>{찾은상품.price}원</p>
+
+                    <Info 재고={props.재고}></Info>
+                    <button className="btn btn-danger" onClick={()=>{ props.재고변경() }}>주문하기</button> 
                     <button className="btn btn-danger" onClick={()=>{
                         history.push('/')
                         }}>뒤로가기</button> 
@@ -23,6 +61,12 @@ function Detail(props){
             </div>
         </div> 
     )
+  }
+
+  function Info(props){
+      return (
+          <p>재고: {props.재고[0]}</p>
+      )
   }
 
   export default Detail;
