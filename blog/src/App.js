15af4248ee0940a6a5aebfff4dc8aa882bebfc7a
yunseongbin <s30ngb1n@gmail.com>
2022-01-08 12:33:14 +0900

    220108

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,25 +1,64 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동 맛집', '파이썬독학']);
+  let [따봉, 따봉변경] = useState(0);
+  let [modal, modal변경] = useState(false);
+  let [누른제목, 누른제목변경] = useState(0);
+
+  let [입력값, 입력값변경] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 Blog</div>
+      </div>
+      
+      {
+        글제목.map(function(글, i){
+          return (
+        <div className="list" key={i}> 
+          <h3 onClick={ ()=> { 누른제목변경(i) }}> { 글 } <span onClick={ ()=> 
+          {따봉변경(따봉 + 1)}}>👍</span> {따봉} </h3>
+          <p>2월 18일 발행</p>
+          <hr />
+        </div>
+          )
+        })
+      }
+
+      <div className="publish">
+        <input onChange={ (e)=>{ 입력값변경(e.target.value)} }/>
+        <button onClick={ ()=>{
+            var arrayCopy = [...글제목]
+            arrayCopy.unshift(입력값); 
+            글제목변경( arrayCopy );
+         }}>저장</button>
+      </div>
+
+
+
+      <input onChange={ (e)=>{ 입력값변경(e.target.value) } } />
+
+
+      <button onClick={() => {modal변경(!modal)}}>버튼</button>
+      {modal === true ? <Modal 글제목={글제목} 누른제목={누른제목}></Modal> : null}
+
     </div>
   );
+}
+
+function Modal(props){
+  return (
+    <div className="modal">
+        <h2>제목 {props.글제목[props.누른제목]} </h2>
+        <p>날짜</p>
+        <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
